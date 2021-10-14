7e702faa633f4a9657dc08b87f56c77c2ea994eb
agustin <agustin.piqueres@gmail.com>
2021-10-14 23:24:20 +0200

    doc: steps to be done

---
+++ ghcp.py
@@ -0,0 +1,10 @@
+"""Main script. 
+
+1. Clone the repository to the current directory.
+2. Copy to another path
+3. Add repo to github (https://kbroman.org/github_tutorial/pages/init.html)
+"""
+
+import subprocess
+import pathlib
+
